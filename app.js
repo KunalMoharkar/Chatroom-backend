d8e3abc0d24105054008e30df5798078d741a6e2
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2021-05-08 17:00:39 +0530

    room backend

--- app.js
+++ app.js
@@ -8,38 +8,23 @@ const io = require('socket.io')(server, {
   });
 
 
-
-
-
-
 io.on('connection', (socket) => { 
 
+socket.on('join-room',(message)=>{
+  const username = message.username;
+  const roomId = message.roomId;
+  console.log(`socket ${socket.id} -- ${username} joining room ${roomId}`)
+  socket.join(roomId);
 
-console.log(socket.id);
+})
 
-const id = socket.id;
-
-console.log("client connected");
-
-//socket.join('room');
-
-console.log("client in room");
-
-//io.to('room').emit('common msg','yeh msg har jagh');
-/*
-io.to('room').emit('newMessage', {
-  from:'jen@mds',
-  text:id,
-  createdAt:123
-});
-*/
 
 socket.on('send-message',function(message){
 
   console.log("message from client");
   console.log(message);
-
-  io.sockets.emit('receive-message', message);
+  const roomId = message.roomId;
+  io.to(roomId).emit('receive-message', message);
 
 });
 
