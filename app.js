5beccedb9456da532259de66fac405da5be91a38
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2021-05-12 21:11:48 +0530

    participants List

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = require('express')();
 const cors = require('cors')
+const short = require('short-uuid');
 const server = require('http').createServer(app);
 
 //req body to json
@@ -22,24 +23,25 @@ let VALIDATIOR = {};
 
 
 //this API route will return the unique room ID.
-//current implementaion just returns the requesting sockets ID as room ID
+//current implementaion just returns the random uinque string  as room ID
 app.post('/getRoomId', (req, res) => {
+
+  //cors
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   console.log(req.body);
   socketId = req.body.socketId;
 
-
-  VALIDATIOR[socketId] = [];
-  VALIDATIOR[socketId].push(socketId);
-
+  //generate a unique room id
+  roomId = short.generate();
+  VALIDATIOR[roomId] = [];
   console.log(VALIDATIOR);
-  console.log(socketId);
-  res.json({'roomId':socketId});
+
+  //send room id to client
+  res.json({roomId:roomId});
+
 })
 
-
-  
 
 //Sockets Events
 io.on('connection', (socket) => { 
@@ -49,21 +51,43 @@ socket.on('join-room',(message)=>{
   const username = message.username;
   const roomId = message.roomId;
 
+  console.log(`usrname - ${username}`)
+  console.log(`room id - ${roomId}`);
+
+  //if room code is correct
   if (VALIDATIOR.hasOwnProperty(roomId)) {
 
+    //push into participants list
+    VALIDATIOR[roomId].push({socketId:socket.id,userName:username});
     console.log(`socket ${socket.id} -- ${username} joining room ${roomId}`)
+    //join room
     socket.join(roomId);
+
+    //send the list to all in a room
+    io.to(roomId).emit('participants',{participantsList:VALIDATIOR[roomId]});
   
   }
   else
   {
 
+    //error msg
     socket.emit('error',{errorMsg:"invalid room code"});
     console.log(`invalid room code`)
 
   }
 
-})
+});
+
+//on disconnection kick the client of the room
+socket.on("disconnecting", (reason) => {
+  let socketRooms = socket.rooms; 
+
+  let socketRoomsArray = socketRooms.entries();
+
+  console.log(socketRoomsArray);
+
+
+});
 
 
 //broadcast message sent by client to its Room
