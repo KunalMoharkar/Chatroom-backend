be9b4b3c481526a91ebea9adadd9fcac915259be
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2021-05-19 23:52:18 +0530

    update cors rules

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors());
 //sockets.io instance
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
